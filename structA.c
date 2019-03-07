4287943c733d0652213a9ac16d5cf72083f4d6e0
kerolayne <kerolaynesouza@outlook.com.br>
2019-03-06 21:28:58 -0300

    Create structA.c

---
+++ structA.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*
+STRUCT
+
+*HETEROGENEAS
+*VALORES SAO ACESSADOS ATRAVES DOS IDENTIFICADORES(NOMES/VARIAVEIS) DOS CAMPOS.
+
+*/
+
+typedef struct conta{
+    int num;
+    float saldo;
+    char cliente[30];
+}Cont;
+
+int main(int argc, char const *argv[]) {
+    Cont x,y;
+
+    x.num = 123;
+    x.saldo = 1000;
+
+    strcpy(x.cliente, "Maria");
+
+    printf("num:%d\nsaldo:R$%.2f\nCliente:%s\n", x.num, x.saldo, x.cliente);
+
+    printf("Digite os valores de y\n");
+
+    scanf("%d %f %s", &y.num, &y.saldo, y.cliente);
+
+    printf("num:%d\nsaldo:R$%.2f\nCliente:%s\n", y.num, y.saldo, y.cliente);
+
+    x=y;//funciona x recebe y
+
+    printf("X:\nnum:%d\nsaldo:R$%.2f\nCliente:%s\n", x.num, x.saldo, x.cliente);
+    return 0;
+}
