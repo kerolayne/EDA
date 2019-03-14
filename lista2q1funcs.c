025d92e04a9c46bcfb549b006e8111442335c6e2
kerolayne <kerolaynesouza@outlook.com.br>
2019-03-13 21:28:02 -0300

    Create lista2q1funcs.c

---
+++ lista2q1funcs.c
@@ -0,0 +1,22 @@
+/*
+
+1) Crie um TAD para representar pontos num plano cartesiano em 2D. A estrutura é composta por
+dois campos representando as coordenadas x e y do ponto. As seguintes operações devem ser
+definidas (.h) e implementadas (.c):
+a. Inicialização: dados o ponto e os valores das coordenadas;
+b. Calcular a distância entre dois pontos: dados dois pontos A(x,y) e B(x,y), conforme a
+seguinte fórmula:
+c. Retornar o valor da coordenada x;
+d. Retornar o valor da coordenada y;
+e. Mostrar na tela os dados de um ponto.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct {
+	int 
+}Vetor;
+void coordenadas(); 
+
+void distancia();
+
+void mostra();
