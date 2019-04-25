cc4bca8cb2d37d24df4e73d70afbbe71e1999bc2
kerolayne <kerolaynesouza@outlook.com.br>
2019-04-24 22:08:43 -0300

    Update MainLista.c

--- MainLista.c
+++ MainLista.c
@@ -1,10 +1,27 @@
 #include "lista.h"
+#include <stdio.h>
+
+void mostra_float(void *x);
 
 int main(int argc, char const *argv[]) {
   Lista l1;
 
+  float x = 9;
+
   inicializa_lista(&l1, sizeof(float));
 
   insereNoInicio(&l1, &x);
+  x = 3;
+
+  removeDoInicio(&l1, &x);
+
+  mostra_lista(l1, mostra_float);
+
   return 0;
+}
+
+
+void mostra_float(void *x){
+  float *p = x;
+  printf("%.1f\n", *p);
 }
