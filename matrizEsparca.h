efaf0e9ab3c211aaaa48a9719c4379e8fc03803b
Kerolayne Souza <kerolaynesouza@outlook.com.br>
2019-06-22 22:34:44 -0300

    Update matrizEsparca.h

--- matrizEsparca.h
+++ matrizEsparca.h
@@ -6,13 +6,20 @@ typedef struct {
   int linha;
   int coluna;
   int valor;
-  
+
 }EntradaMatriz;
 
 typedef struct  {
   Lista m;
   int nLinha;
   int nColuna;
-  
+
 }MatrizEsparsa;
 
+int inicializa_matriz(Lista m, int l, int c);
+
+void mostra_matriz(MatrizEsparca mat);
+
+void lePosicaoMatriz(MatrizEsparca &matriz, int linha, int coluna);
+
+int insereNaPosicaoMatriz(MatrizEsparca &matriz, int linha, int coluna);
